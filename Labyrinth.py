e93a317757e59e0d7b1b15bfc3ee02212a53b33b
Timofey <vsjo.zanjato@gmail.com>
2016-09-19 20:57:35 +0300

    Add validation input data (height and width)

--- Labyrinth.py
+++ Labyrinth.py
@@ -7,11 +7,14 @@ def main(argv):
     parser.add_argument("--h", type=int, help="height labyrinth")
     parser.add_argument("--seed", type=int, default=0, help="random key")
     args = parser.parse_args()
+    if args.h == None or args.w == None:
+        print("You must enter the dimensions of the labyrinth")
+        return
+    # build labyrinth
+    w = args.w
     h = args.h
     seed = args.seed
-    print("height %d" % args.h)
-    print("seed %d" % args.seed)
-
+    
 if __name__ == "__main__":
     import sys
     main(sys.argv)
