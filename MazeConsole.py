ac19f8e349ee10ba1f53e0fdd8d47f109f1b0b62
Timofey <vsjo.zanjato@gmail.com>
2016-10-03 23:18:35 +0300

    Not, not all bags fixed :(

--- MazeConsole.py
+++ MazeConsole.py
@@ -9,13 +9,14 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--w", type=int, default=4, help="width maze")
     parser.add_argument("--h", type=int, default=4, help="height maze")
-    parser.add_argument("--seed", type=int, default=random.random(), help="random key")
+    parser.add_argument("--seed", type=int, default=3, help="random key")
     args = parser.parse_args()
     w = args.w
     h = args.h
     seed = args.seed
     maze = Maze(w, h, seed)
     maze_matrix = maze.generate_maze_data(w, h, seed)
+    build_maze_from_matrix(maze_matrix)
 
 
 
